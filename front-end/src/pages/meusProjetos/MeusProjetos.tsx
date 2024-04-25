02328d2d7036a1878543da16773ab1370e934325
Fernando Davi <92491040+fnddavi@users.noreply.github.com>
2024-04-24 22:55:55 -0300

    Uppackjson meusProjetos

--- front-end/src/pages/meusProjetos/MeusProjetos.tsx
+++ front-end/src/pages/meusProjetos/MeusProjetos.tsx
@@ -1,22 +1,65 @@
-import React from 'react';
-import { Box } from '@mui/system';
-import { useTheme } from '@mui/material/styles';
-import useMediaQuery from '@mui/material/useMediaQuery';
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { Stack } from '@mui/material';
 
-const MeusProjetos: React.FC = () => {
-    const theme = useTheme();
-    const smDown = useMediaQuery(theme.breakpoints.down('sm'));
 
+function createData(
+    name: string,
+    calories: number,
+    fat: number,
+    carbs: number,
+    proteinas: number,
+) {
+    return { name, calories, fat, carbs, proteinas };
+}
+
+const rows = [
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+export default function MeusProjetos() {
     return (
-        <>
-        <Box
-            height="90vh"
-            paddingLeft={smDown ? theme.spacing(4) : theme.spacing(35)} // Use a variável smDown para determinar o valor do espaçamento
-        >
-            <h1>MEUS PROJETOS</h1>
-        </Box>
-        </>
+        <Stack spacing={3} margin="2% auto" marginLeft="20%">
+            <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                    <TableHead>
+                        <h2>Título da nova tabela</h2>
+                        <TableRow>
+                            <TableCell>Dessert (100g serving)</TableCell>
+                            <TableCell align="right">Calories</TableCell>
+                            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows.map((row) => (
+                            <TableRow
+                                key={row.name}
+                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                            >
+                                <TableCell component="th" scope="row">
+                                    {row.name}
+                                </TableCell>
+                                <TableCell align="right">{row.calories}</TableCell>
+                                <TableCell align="right">{row.fat}</TableCell>
+                                <TableCell align="right">{row.carbs}</TableCell>
+                                <TableCell align="right">{row.proteinas}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </Stack>
     );
-};
-
-export default MeusProjetos;
+}
