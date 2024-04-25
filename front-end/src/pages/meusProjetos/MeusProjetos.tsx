1843b86e4cdb21436035a0a243d4fff8a9ba6622
Fernando Davi <92491040+fnddavi@users.noreply.github.com>
2024-04-24 23:55:47 -0300

    Update-paginaMeusProjetos

--- front-end/src/pages/meusProjetos/MeusProjetos.tsx
+++ front-end/src/pages/meusProjetos/MeusProjetos.tsx
@@ -7,39 +7,41 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { Stack } from '@mui/material';
+import  Typography from '@mui/material/Typography';
+
 
 
 function createData(
+    ID: number,
     name: string,
-    calories: number,
-    fat: number,
-    carbs: number,
-    proteinas: number,
+    data: Date,
+    percent: number,
+
 ) {
-    return { name, calories, fat, carbs, proteinas };
+    return { ID, name, data, percent };
 }
 
 const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData(1, 'Juliana', new Date('2021-09-15'), 6.0),
+    createData(2, 'Abner', new Date('2021-09-15'), 9.0),
+    createData(3, 'Claudia', new Date('2021-09-15'), 16.0),
+    createData(4, 'Michael', new Date('2021-09-15'), 3.7),
+    createData(5, 'Fernando', new Date('2021-09-15'), 16.0),
 ];
 
-export default function MeusProjetos() {
+export default function TabelaProjetos() {
     return (
         <Stack spacing={3} margin="2% auto" marginLeft="20%">
+            <Typography variant="h2" sx={{ fontFamily: 'DM Sans', fontSize: '20px' }}>Título da nova tabela</Typography>
             <TableContainer component={Paper}>
                 <Table sx={{ minWidth: 650 }} aria-label="simple table">
                     <TableHead>
-                        <h2>Título da nova tabela</h2>
+                        
                         <TableRow>
-                            <TableCell>Dessert (100g serving)</TableCell>
-                            <TableCell align="right">Calories</TableCell>
-                            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                            <TableCell>ID</TableCell>
+                            <TableCell align="right">Nome</TableCell>
+                            <TableCell align="right">Data</TableCell>
+                            <TableCell align="right">% Concluido</TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
@@ -48,13 +50,11 @@ export default function MeusProjetos() {
                                 key={row.name}
                                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                             >
-                                <TableCell component="th" scope="row">
-                                    {row.name}
-                                </TableCell>
-                                <TableCell align="right">{row.calories}</TableCell>
-                                <TableCell align="right">{row.fat}</TableCell>
-                                <TableCell align="right">{row.carbs}</TableCell>
-                                <TableCell align="right">{row.proteinas}</TableCell>
+                                <TableCell component="th" scope="row"> {row.ID} </TableCell>
+                                <TableCell align="right">{row.name}</TableCell>
+                                <TableCell align="right">{row.data.toLocaleDateString()}</TableCell>
+                                <TableCell align="right">{row.percent}</TableCell>
+
                             </TableRow>
                         ))}
                     </TableBody>
