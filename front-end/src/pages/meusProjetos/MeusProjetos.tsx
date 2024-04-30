8e2d7d6f0eaec3088c3e40c59e92b9bf1596a94e
fnd.davi <fernando.ferreira32@fatec.sp.gov.br>
2024-04-29 22:38:52 -0300

    Update MeusProjetos. Services e Types

--- front-end/src/pages/meusProjetos/MeusProjetos.tsx
+++ front-end/src/pages/meusProjetos/MeusProjetos.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -8,45 +8,48 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { Stack } from '@mui/material';
 import Typography from '@mui/material/Typography';
+import { fetchMunicipios } from '../../services/projetos';
+import { MunicipioProps } from '../../types/meusProjetos';
 
-function createData(
-    ID: number,
-    name: string,
-    data: Date,
-    percent: number,
-) {
-    return { ID, name, data, percent };
-}
 
-const rows = [
-    createData(1, 'Juliana', new Date('2021-09-15'), 6.0),
-    createData(2, 'Abner', new Date('2021-09-15'), 9.0),
-    createData(3, 'Claudia', new Date('2021-09-15'), 16.0),
-    createData(4, 'Michael', new Date('2021-09-15'), 3.7),
-    createData(5, 'Fernando', new Date('2021-09-15'), 16.0),
-];
 
-export default function TabelaProjetos() {
+
+function TabelaMunicipios() {
+    const [municipios, setMunicipios] = useState([]);
+
+    useEffect(() => {
+        const fetchMunicipiosData = async () => {
+            try {
+                const data = await fetchMunicipios();
+                setMunicipios(data);
+            } catch (error) {
+                console.error('Erro ao buscar os municípios:', error);
+            }
+        };
+
+        fetchMunicipiosData();
+    }, []);
+
     return (
         <Stack spacing={3} margin="3% auto" marginLeft="20%">
-            <Typography variant="h4">Meus Projetos</Typography>
+            <Typography variant="h4">Municípios</Typography>
             <TableContainer component={Paper} sx={{ maxWidth: 1000 }}>
                 <Table sx={{ minWidth: 600 }} aria-label="simple table">
                     <TableHead>
                         <TableRow>
-                            <TableCell style={{ minWidth: 50 }} align="center">ID</TableCell>
-                            <TableCell style={{ minWidth: 100 }} align="center">Nome</TableCell>
-                            <TableCell style={{ minWidth: 100 }} align="center">Data</TableCell>
-                            <TableCell style={{ minWidth: 150 }} align="center"> % Concluido</TableCell>
+                            <TableCell>ID</TableCell>
+                            <TableCell>Nome</TableCell>
+                            <TableCell>Código</TableCell>
+                            <TableCell>UF</TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {rows.map((row) => (
-                            <TableRow key={row.name}>
-                                <TableCell align="center">{row.ID}</TableCell>
-                                <TableCell align="center">{row.name}</TableCell>
-                                <TableCell align="center">{row.data.toLocaleDateString()}</TableCell>
-                                <TableCell align="center">{row.percent}</TableCell>
+                        {municipios.map((municipio: MunicipioProps) => (
+                            <TableRow key={municipio._id}>
+                                <TableCell>{municipio._id}</TableCell>
+                                <TableCell>{municipio.nm_mun}</TableCell>
+                                <TableCell>{municipio.cd_mun}</TableCell>
+                                <TableCell>{municipio.sigla_uf}</TableCell>
                             </TableRow>
                         ))}
                     </TableBody>
@@ -55,3 +58,5 @@ export default function TabelaProjetos() {
         </Stack>
     );
 }
+
+export default TabelaMunicipios;
---
+++ front-end/src/types/meusProjetos.ts
@@ -0,0 +1,6 @@
+export interface MunicipioProps {
+    _id: string;
+    nm_mun: string;
+    cd_mun: string;
+    sigla_uf: string;
+}
