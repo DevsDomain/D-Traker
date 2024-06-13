3e50c6de8c9bd7d1970d5e123763b55ed3d00b0f
abnercosta97 <abner.costa3@fatec.sp.gov.br>
2024-06-12 22:34:25 -0300

    ok

--- front-end/src/components/graficos/GraficoBarra.tsx
+++ front-end/src/components/graficos/GraficoBarra.tsx
@@ -1,124 +1,165 @@
-import React, { useState, useEffect } from 'react';
-import Box from '@mui/material/Box';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
-import Typography from '@mui/material/Typography';
-import { ProjetoStatus } from '../../types/projetos';
-import { fetchPoligonos } from '../../services/poligonos';
-import { api } from '../../services';
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, { useState, useEffect } from "react";
+import Box from "@mui/material/Box";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
+import Typography from "@mui/material/Typography";
+import { ProjetoStatus } from "../../types/projetos";
+import { fetchPoligonos } from "../../services/poligonos";
+import useAuth from "../../hooks/auth";
 
 // Defina o tipo GetSeriesParams
 type GetSeriesParams = {
-    hasNegativeValue: boolean;
-    stackOffset: string;
+  hasNegativeValue: boolean;
+  stackOffset: string;
 };
 
-const availableStackOffset = ['Atibaia', 'Cruzeiro', 'Taubaté'] as const;
+const availableStackOffset = ["Atibaia", "Cruzeiro", "Taubaté"] as const;
 
-type StackOffset = typeof availableStackOffset[number];
+type StackOffset = (typeof availableStackOffset)[number];
 
 // Defina o tipo Projeto
 type Projeto = {
-    Projeto: string;
-    NomeProjeto: string;
-    "Não Atribuido": string;
-    "Em andamento": string;
-    "Concluido": string;
+  Projeto: string;
+  NomeProjeto: string;
+  "Não Atribuido": string;
+  "Em andamento": string;
+  Concluido: string;
 };
 
-const fetchProjectsData = async ({ andamento, concluidos, naoAtribuido } : ProjetoStatus): Promise<Projeto[]> => {
-    const apiResponse = await fetchPoligonos();
-    console.log(apiResponse);
-
-    return [
-        {
-            Projeto: apiResponse[0].projeto,
-            NomeProjeto: apiResponse[0].nomeProjeto,
-            "Não Atribuido": apiResponse[0].naoAtribuido,
-            "Em andamento": apiResponse[0].andamento,
-            "Concluido": apiResponse[0].concluidos,
-        },
-        {
-            Projeto: apiResponse[1].projeto,
-            NomeProjeto: apiResponse[1].nomeProjeto,
-            "Não Atribuido": apiResponse[1].naoAtribuido,
-            "Em andamento": apiResponse[1].andamento,
-            "Concluido": apiResponse[1].concluidos,
-        },
-        {
-            Projeto: apiResponse[2].projeto,
-            NomeProjeto: apiResponse[2].nomeProjeto,
-            "Não Atribuido": apiResponse[2].naoAtribuido,
-            "Em andamento": apiResponse[2].andamento,
-            "Concluido": apiResponse[2].concluidos,
-        },
+const fetchProjectsData = async (
+  idProjeto: string,
+  role: string
+): Promise<Projeto[]> => {
+  let apiResponse = await fetchPoligonos();
+  console.log("aqui agora", apiResponse);
+  let resultados: Projeto[] = [];
+  if (role !== "adm") {
+    console.log("antes", idProjeto);
+    apiResponse = apiResponse.filter(
+      (projeto: any) => Number(projeto.projeto) === Number(idProjeto)
+    );
+    console.log(" agora", apiResponse);
+    resultados = [
+      {
+        Projeto: apiResponse[0].projeto,
+        NomeProjeto: apiResponse[0].nomeProjeto,
+        "Não Atribuido": apiResponse[0].naoAtribuido,
+        "Em andamento": apiResponse[0].andamento,
+        Concluido: apiResponse[0].concluidos,
+      },
     ];
+  }
+  if (role === "adm") {
+    resultados = [
+      {
+        Projeto: apiResponse[0].projeto,
+        NomeProjeto: apiResponse[0].nomeProjeto,
+        "Não Atribuido": apiResponse[0].naoAtribuido,
+        "Em andamento": apiResponse[0].andamento,
+        Concluido: apiResponse[0].concluidos,
+      },
+      {
+        Projeto: apiResponse[1].projeto,
+        NomeProjeto: apiResponse[1].nomeProjeto,
+        "Não Atribuido": apiResponse[1].naoAtribuido,
+        "Em andamento": apiResponse[1].andamento,
+        Concluido: apiResponse[1].concluidos,
+      },
+      {
+        Projeto: apiResponse[2].projeto,
+        NomeProjeto: apiResponse[2].nomeProjeto,
+        "Não Atribuido": apiResponse[2].naoAtribuido,
+        "Em andamento": apiResponse[2].andamento,
+        Concluido: apiResponse[2].concluidos,
+      },
+    ];
+  }
+  return resultados;
 };
 
-const getSeries = (projetos: Projeto[], { hasNegativeValue, stackOffset }: GetSeriesParams) => {
-    return projetos.map(projeto => ({
-        name: projeto.NomeProjeto,
-        "Não Atribuido": projeto["Não Atribuido"],
-        "Em andamento": projeto["Em andamento"],
-        "Concluido": projeto["Concluido"],
-    }));
+const getSeries = (
+  projetos: Projeto[],
+  { hasNegativeValue, stackOffset }: GetSeriesParams
+) => {
+  return projetos.map((projeto) => ({
+    name: projeto.NomeProjeto,
+    "Não Atribuido": projeto["Não Atribuido"],
+    "Em andamento": projeto["Em andamento"],
+    Concluido: projeto["Concluido"],
+  }));
 };
 
 interface GraficoBarrasProps {
-    andamento: string;
-    concluidos: string;
-    naoAtribuido: string;
+  andamento: string;
+  concluidos: string;
+  naoAtribuido: string;
 }
 
-const GraficoBarras: React.FC<GraficoBarrasProps> = ({ andamento, concluidos, naoAtribuido }) => {
-    console.log(andamento, concluidos, naoAtribuido);
-    const [stackOffset, setStackOffset] = useState<string>('Gráfico');
-    const [hasNegativeValue, setHasNegativeValue] = useState<boolean>(false); // Assume that there are no negative values initially
-    const [projetos, setProjetos] = useState<Projeto[]>([]);
+const GraficoBarras: React.FC<GraficoBarrasProps> = ({
+  andamento,
+  concluidos,
+  naoAtribuido,
+}) => {
+  const { user } = useAuth();
+  console.log(andamento, concluidos, naoAtribuido);
+  const [stackOffset, setStackOffset] = useState<string>("Gráfico");
+  const [hasNegativeValue, setHasNegativeValue] = useState<boolean>(false); // Assume that there are no negative values initially
+  const [projetos, setProjetos] = useState<Projeto[]>([]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const data = await fetchProjectsData({ andamento, concluidos, naoAtribuido });
-                setProjetos(data);
-            } catch (error) {
-                console.error("Erro ao buscar os projetos:", error);
-            }
-        };
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchProjectsData(user.idProjeto, user.role);
+        setProjetos(data);
+      } catch (error) {
+        console.error("Erro ao buscar os projetos:", error);
+      }
+    };
 
-        fetchData();
-    }, []);
+    fetchData();
+  }, []);
 
-    const chartWidth = 300;
-    const chartHeight = 300;
-    const chartMargin = 20;
+  const chartWidth = 300;
+  const chartHeight = 300;
+  const chartMargin = 20;
 
-    return (
-        <Box sx={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
-            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                <Typography variant="h5">Polígonos</Typography>
-                <BarChart
-                    width={chartWidth}
-                    height={chartHeight}
-                    data={getSeries(projetos, { hasNegativeValue, stackOffset })}
-                    margin={{
-                        top: 20,
-                        right: chartMargin,
-                        bottom: 5,
-                        left: chartMargin,
-                    }}
-                >
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                    <Tooltip />
-                    <Legend />
-                    <Bar dataKey="Não Atribuido" stackId="a" fill="#D941CF" />
-                    <Bar dataKey="Em andamento" stackId="a" fill="#5854BF" />
-                    <Bar dataKey="Concluido" stackId="a" fill="#1BF28E" />
-                </BarChart>
-            </Box>
-        </Box>
-    );
+  return (
+    <Box sx={{ width: "100%", display: "flex", justifyContent: "center" }}>
+      <Box
+        sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+      >
+        <Typography variant="h5">Polígonos</Typography>
+        <BarChart
+          width={chartWidth}
+          height={chartHeight}
+          data={getSeries(projetos, { hasNegativeValue, stackOffset })}
+          margin={{
+            top: 20,
+            right: chartMargin,
+            bottom: 5,
+            left: chartMargin,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="Não Atribuido" stackId="a" fill="#D941CF" />
+          <Bar dataKey="Em andamento" stackId="a" fill="#5854BF" />
+          <Bar dataKey="Concluido" stackId="a" fill="#1BF28E" />
+        </BarChart>
+      </Box>
+    </Box>
+  );
 };
 
 export default GraficoBarras;
--- front-end/src/pages/Dashboard/index.tsx
+++ front-end/src/pages/Dashboard/index.tsx
@@ -1,89 +1,149 @@
 // Dashboard.tsx
-import React from 'react';
-import { Grid, Stack } from '@mui/material';
-import { useTheme } from '@mui/material/styles';
-import useMediaQuery from '@mui/material/useMediaQuery';
-import GraficoBarras from '../../components/graficos/GraficoBarra';
-import GraficoPizza from '../../components/graficos/GraficoStatusProjeto';
-import { TasksProgress } from '../../components/Box/tasks_progress';
-import GraficoDeApontamentos from '../../components/graficos/GraficoLinha';
-import BasicSelect from '../../components/Select';
-import { DashboardProvider, useDashboard } from '../../contexts/dashboardContext';
-import { TotalTasks } from '../../components/Box/total_tasks';
-import { TotalPoligonos } from '../../components/Box/total_poligonos';
-import { AreaMapeada } from '../../components/Box/area_mapeada';
+import React from "react";
+import { Grid, Stack } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import GraficoBarras from "../../components/graficos/GraficoBarra";
+import GraficoPizza from "../../components/graficos/GraficoStatusProjeto";
+import { TasksProgress } from "../../components/Box/tasks_progress";
+import GraficoDeApontamentos from "../../components/graficos/GraficoLinha";
+import BasicSelect from "../../components/Select";
+import {
+  DashboardProvider,
+  useDashboard,
+} from "../../contexts/dashboardContext";
+import { TotalTasks } from "../../components/Box/total_tasks";
+import { TotalPoligonos } from "../../components/Box/total_poligonos";
+import { AreaMapeada } from "../../components/Box/area_mapeada";
+import { Box } from "@mui/system";
 
 const DashboardContent: React.FC = () => {
-    const theme = useTheme();
-    const smDown = useMediaQuery(theme.breakpoints.down('sm'));
-    const { state, setFiltroMembros, setFiltroProjetos } = useDashboard();
-    const { projetoStatus } = state
-    const handleProjetoChange = (value: string) => {
-        setFiltroProjetos(value);
-    };
+  const theme = useTheme();
+  const smDown = useMediaQuery(theme.breakpoints.down("sm"));
+  const { state, setFiltroMembros, setFiltroProjetos } = useDashboard();
+  const { projetoStatus } = state;
+  const handleProjetoChange = (value: string) => {
+    setFiltroProjetos(value);
+  };
 
-    const handleMembroChange = (value: string) => {
-        setFiltroMembros(value);
-    };
-    return (
-        <Box overflow="hidden">
-            <Stack width={'50vw'} direction={"row"} marginLeft={60}>
-                <BasicSelect projeto={state.projetos} handlePesquisaByProjeto={setFiltroProjetos} onChange={handleProjetoChange} placeHolder='Projetos' value={state.selectedProject || ""} />
-            </Stack>
+  const handleMembroChange = (value: string) => {
+    setFiltroMembros(value);
+  };
+  return (
+    <Box overflow="hidden">
+      <Stack width={"50vw"} direction={"row"} marginLeft={60}>
+        <BasicSelect
+          projeto={state.projetos}
+          handlePesquisaByProjeto={setFiltroProjetos}
+          onChange={handleProjetoChange}
+          placeHolder="Projetos"
+          value={state.selectedProject || ""}
+        />
+      </Stack>
 
-            <Grid container spacing={2} marginLeft={smDown ? 0 : theme.spacing(35)} marginTop={smDown ? 0 : theme.spacing(3)}>
-                <Grid item lg={2.3} sm={6} xs={12}>
-                    <TotalTasks value="Total" naoAtribuido={projetoStatus.naoAtribuido} sx={{ height: '100%', width: '80%' }} />
-                </Grid>
+      <Grid
+        container
+        spacing={2}
+        marginLeft={smDown ? 0 : theme.spacing(35)}
+        marginTop={smDown ? 0 : theme.spacing(3)}
+      >
+        <Grid item lg={2.3} sm={6} xs={12}>
+          <TotalTasks
+            value="Total"
+            naoAtribuido={projetoStatus.naoAtribuido}
+            sx={{ height: "100%", width: "80%" }}
+          />
+        </Grid>
 
-                <Grid item lg={2.3} sm={6} xs={12}>
-                    <TotalPoligonos
-                        andamento={projetoStatus.andamento}
-                        concluidos={projetoStatus.concluidos}
-                        naoAtribuido={projetoStatus.naoAtribuido}
-                        sx={{ height: '100%', width: '80%' }}
-                    />
-                </Grid>
+        <Grid item lg={2.3} sm={6} xs={12}>
+          <TotalPoligonos
+            andamento={projetoStatus.andamento}
+            concluidos={projetoStatus.concluidos}
+            naoAtribuido={projetoStatus.naoAtribuido}
+            sx={{ height: "100%", width: "80%" }}
+          />
+        </Grid>
 
-                <Grid item lg={2.3} sm={6} xs={12}>
-                    <TasksProgress andamento={projetoStatus.andamento} concluidos={projetoStatus.concluidos} naoAtribuido={projetoStatus.naoAtribuido} />
-                </Grid>
+        <Grid item lg={2.3} sm={6} xs={12}>
+          <TasksProgress
+            andamento={projetoStatus.andamento}
+            concluidos={projetoStatus.concluidos}
+            naoAtribuido={projetoStatus.naoAtribuido}
+          />
+        </Grid>
 
-                <Grid item lg={2.3} sm={6} xs={12}>
-                    <AreaMapeada sx={{ height: '100%', width: '80%' }} />
-                </Grid>
+        <Grid item lg={2.3} sm={6} xs={12}>
+          <AreaMapeada sx={{ height: "100%", width: "80%" }} />
+        </Grid>
+      </Grid>
 
-            </Grid>
+      <Grid
+        container
+        spacing={0}
+        marginLeft={smDown ? 0 : theme.spacing(30)}
+        marginTop={smDown ? 0 : theme.spacing(5)}
+      >
+        <Grid item xs={3} sm={3}>
+          <GraficoPizza
+            andamento={projetoStatus.andamento}
+            concluidos={projetoStatus.concluidos}
+            naoAtribuido={projetoStatus.naoAtribuido}
+          />
+        </Grid>
+        <Grid item xs={3} sm={3}>
+          <GraficoPizza
+            andamento={projetoStatus.andamento}
+            concluidos={projetoStatus.concluidos}
+            naoAtribuido={projetoStatus.naoAtribuido}
+          />
+        </Grid>
+        <Grid item xs={3} sm={3}>
+          <GraficoBarras
+            andamento={projetoStatus.andamento}
+            naoAtribuido={projetoStatus.naoAtribuido}
+            concluidos={projetoStatus.concluidos}
+          />
+        </Grid>
 
-            <Grid container spacing={0} marginLeft={smDown ? 0 : theme.spacing(30)} marginTop={smDown ? 0 : theme.spacing(5)}>
-                <Grid item xs={3} sm={3}>
-                    <GraficoPizza andamento={projetoStatus.andamento} concluidos={projetoStatus.concluidos}
-                        naoAtribuido={projetoStatus.naoAtribuido} />
-                </Grid>
-                <Grid item xs={3} sm={3}>
-                    <GraficoPizza andamento={projetoStatus.andamento} concluidos={projetoStatus.concluidos}
-                        naoAtribuido={projetoStatus.naoAtribuido} />
-                </Grid>
-                <Grid item xs={3} sm={3}>
-                    <GraficoBarras andamento={projetoStatus.andamento} naoAtribuido={projetoStatus.naoAtribuido}
-                        concluidos={projetoStatus.concluidos} />
-                </Grid>
+        <Grid
+          item
+          xs={20}
+          sm={3}
+          marginLeft={smDown ? 0 : theme.spacing(5)}
+          marginTop={smDown ? 0 : theme.spacing(15)}
+        >
+          <BasicSelect
+            projeto={state.projetos}
+            handlePesquisaByProjeto={setFiltroProjetos}
+            onChange={handleProjetoChange}
+            placeHolder="Projetos"
+            value={state.selectedProject || ""}
+          />
 
-                <Grid item xs={20} sm={3} marginLeft={smDown ? 0 : theme.spacing(5)} marginTop={smDown ? 0 : theme.spacing(15)}>
-                    <BasicSelect projeto={state.projetos} handlePesquisaByProjeto={setFiltroProjetos} onChange={handleProjetoChange} placeHolder='Projetos' value={state.selectedProject || ""} />
-
-                    <BasicSelect projeto={state.membros} handlePesquisaByProjeto={setFiltroMembros} onChange={handleMembroChange} placeHolder='Membros' value={state.selectedMember || ""} />
-                    <GraficoDeApontamentos alteracoes={state.filteredAlteracoes.length === 0 ? state.alteracoes : state.filteredAlteracoes} />
-                </Grid>
-            </Grid>
-        </Box>
-    );
+          <BasicSelect
+            projeto={state.membros}
+            handlePesquisaByProjeto={setFiltroMembros}
+            onChange={handleMembroChange}
+            placeHolder="Membros"
+            value={state.selectedMember || ""}
+          />
+          <GraficoDeApontamentos
+            alteracoes={
+              state.filteredAlteracoes.length === 0
+                ? state.alteracoes
+                : state.filteredAlteracoes
+            }
+          />
+        </Grid>
+      </Grid>
+    </Box>
+  );
 };
 
 const Dashboard: React.FC = () => (
-    <DashboardProvider>
-        <DashboardContent />
-    </DashboardProvider>
+  <DashboardProvider>
+    <DashboardContent />
+  </DashboardProvider>
 );
 
 export default Dashboard;
